1205f47abc28dfd4fb7b5fe55d714fb48a20c943
salilpal <salilp379@gmail.com>
2025-06-04 19:39:49 +0530

    added footer

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+      <div class="flex my-2.5 mx-50 items-center justify-between py-4 border-t border-[#797979]">
+        <p>© 2025 Edusity. All rights reserved.</p>
+        <ul class="flex justify-between items-center gap-4">
+          <li>Terms of Services</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default footer;
